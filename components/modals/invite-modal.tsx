1024f3c6f2e3ebfa6b813bc34ea9d54fe7c421d0
Sebastian Flores <sebastianflores@ihfintech.com.pe>
2024-03-05 00:29:26 -0500

    feat: invitations

---
+++ components/modals/invite-modal.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import { useModal } from "@/hooks/use-modal-store";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Check, Copy, RefreshCw } from "lucide-react";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
+import axios from "axios";
+
+function InviteModal() {
+  const { onOpen, isOpen, onClose, type, data } = useModal();
+  const origin = useOrigin();
+
+  const isModalOpen = isOpen && type === "invite";
+  const { server } = data;
+
+  const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+  function onCopy() {
+    navigator.clipboard.writeText(inviteUrl);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  }
+
+  async function onNew() {
+    try {
+      setIsLoading(true);
+      const response = await axios.patch(
+        `/api/servers/${server?.id}/invite-code`,
+      );
+      onOpen("invite", { server: response.data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="overflow-auto bg-white p-0 text-black">
+        <DialogHeader className="px-6 pt-8">
+          <DialogTitle className="text-center text-2xl font-bold">
+            Invite Friends
+          </DialogTitle>
+        </DialogHeader>
+        <div className="p-6">
+          <Label className="dark:text-secondary/70 text-xs font-bold uppercase text-zinc-500">
+            Server invite link
+          </Label>
+          <div className="mt-2 flex items-center gap-x-2">
+            <Input
+              disabled={isLoading}
+              className="border-0 bg-zinc-300/50 text-black focus-visible:ring-0 focus-visible:ring-offset-0"
+              value={inviteUrl}
+            />
+            <Button onClick={onCopy} disabled={isLoading} size="icon">
+              {copied ? (
+                <Check className="size-4" />
+              ) : (
+                <Copy className="size-4" />
+              )}
+            </Button>
+          </div>
+          <Button
+            variant="link"
+            size="sm"
+            disabled={isLoading}
+            className="mt-4 text-xs text-zinc-500"
+            onClick={onNew}
+          >
+            Generate a new link
+            <RefreshCw className="ml-2 size-4" />
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default InviteModal;
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -18,6 +18,7 @@ import {
   UserPlus,
   Users,
 } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
@@ -25,53 +26,57 @@ interface ServerHeaderProps {
 }
 
 function ServerHeader({ server, role }: ServerHeaderProps) {
+  const { onOpen } = useModal();
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus:outline-none" asChild>
-        <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+        <button className="text-md flex h-12 w-full items-center border-b-2 border-neutral-200 px-3 font-semibold transition hover:bg-zinc-700/10 dark:border-neutral-800 dark:hover:bg-zinc-700/50">
           {server.name}
-          <ChevronDown className="w-5 h-5 ml-auto" />
+          <ChevronDown className="ml-auto h-5 w-5" />
         </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
+      <DropdownMenuContent className="w-56 space-y-[2px] text-xs font-medium text-black dark:text-neutral-400">
         {isModerator && (
-          <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("invite", { server })}
+            className="cursor-pointer px-3 py-2 text-sm text-indigo-600 dark:text-indigo-400"
+          >
             Invite People
-            <UserPlus className="w-4 h-4 ml-auto" />
+            <UserPlus className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm">
             Server Settings
-            <Settings className="w-4 h-4 ml-auto" />
+            <Settings className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm">
             Manage Members
-            <Users className="w-4 h-4 ml-auto" />
+            <Users className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm">
             Create Channel
-            <PlusCircle className="w-4 h-4 ml-auto" />
+            <PlusCircle className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
         )}
         {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm text-rose-500">
             Delete Server
-            <Trash className="w-4 h-4 ml-auto" />
+            <Trash className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
         )}
         {!isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm text-rose-500">
             Leave Server
-            <LogOut className="w-4 h-4 ml-auto" />
+            <LogOut className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
         )}
       </DropdownMenuContent>
