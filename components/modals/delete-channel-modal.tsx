a62f447b629110c91003ba13f485acd88a18ff77
Sebastian Flores <sebastianflores@ihfintech.com.pe>
2024-03-11 00:16:48 -0500

    feat: edit channel

---
+++ components/modals/delete-channel-modal.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import qs from "query-string";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import { useModal } from "@/hooks/use-modal-store";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+function DeleteChannelModal() {
+  const router = useRouter();
+  const { isOpen, onClose, type, data } = useModal();
+
+  const isModalOpen = isOpen && type === "deleteChannel";
+  const { server, channel } = data;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function onClick() {
+    try {
+      setIsLoading(true);
+
+      const url = qs.stringifyUrl({
+        url: `/api/channels/${channel?.id}`,
+        query: {
+          serverId: server?.id,
+        },
+      });
+
+      await axios.delete(url);
+
+      router.push(`/servers/${server?.id}`);
+      router.refresh();
+      onClose();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="overflow-auto bg-white p-0 text-black">
+        <DialogHeader className="px-6 pt-8">
+          <DialogTitle className="text-center text-2xl font-bold">
+            Delete Channel
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Are you sure you want to do this? <br />
+            <span className="font-semibold text-indigo-500">
+              #{channel?.name}
+            </span>{" "}
+            will be permanently deleted.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex items-center justify-between w-full">
+            <Button disabled={isLoading} onClick={onClose} variant="ghost">
+              Cancel
+            </Button>
+            <Button disabled={isLoading} onClick={onClick} variant="primary">
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default DeleteChannelModal;
